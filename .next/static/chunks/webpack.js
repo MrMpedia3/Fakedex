df9abe1a70792d4fb931884e5ea8e2e20002b539
MrMpedia3 <mrmpedia@gmail.com>
2025-12-03 14:19:39 -0300

    Misseorite art added.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7cfadc1190095de5"; }
+/******/ 		__webpack_require__.h = function() { return "b64152b97a3868de"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

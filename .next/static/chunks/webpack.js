9cff997c4aee1a0081cc6bc3931b971d019af5a6
MrMpedia3 <mrmpedia@gmail.com>
2025-12-10 15:10:35 -0300

    Artes placeholder do Shynarine e Penumbrike adicionadas... credo.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e39aa55733e61035"; }
+/******/ 		__webpack_require__.h = function() { return "c5580ab78917039c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

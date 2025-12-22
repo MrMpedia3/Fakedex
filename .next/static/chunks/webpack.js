3ed38010643c6066b4983fbd99e88e467bfda1d0
MrMpedia3 <mrmpedia@gmail.com>
2025-12-21 22:06:50 -0300

    Bicho do Jay adicionado.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef7fa1d413b0e3d9"; }
+/******/ 		__webpack_require__.h = function() { return "6032cb10faf46bb1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

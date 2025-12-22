fbfd0711b9f2f55dca51f9e94fdc29c97046373f
MrMpedia3 <mrmpedia@gmail.com>
2025-12-22 19:25:30 -0300

    Adicionadas as artes placeholder dos pôneis que faltavam, do Eternalom e Saberatus... calma que já vem arte de verdade.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b980112a080214a3"; }
+/******/ 		__webpack_require__.h = function() { return "4546873924e281b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
